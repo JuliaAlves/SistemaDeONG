cb084ab863f9bb71ebb74fc73abac62374131595
Julia Alves <julieta.a.arruda@gmail.com>
2016-10-01 20:36:51 -0300

    Menu após login

---
+++ menuCadastrado.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="estilo.css">
+        <div id="menu" class="menu-container">
+        <ul class="menu clearfix">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Como Ajudar</a>
+                <ul class="sub-menu clearfix">
+                    <li><a href="#">Voluntariado</a></li>
+                    <li><a href="#">Doações</a></li>
+                    <li><a href="#">Distribuições</a></li>
+                </ul>
+            <li><a href="#">Fale conosco</a>
+                <ul class="sub-menu clearfix">
+                    <li><a href="#">Localização</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Doe Agora</a></li>
+            <li><a href="#">Minha Area</a></li>
+        </ul>
+        </div>
+</head>
+<body>
+
+</body>
+</html>
