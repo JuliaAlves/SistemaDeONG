25bb0a311772e6b4c381a9c00584cbeb5d1b47ba
LarissaSenaBarbosa <larissasenabarbosa31@gmail.com>
2016-10-03 08:46:29 -0300

    Add files via upload
    oi, julia

---
+++ cadastro.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="estiloCadastro.css">
+	</head>
+	<body>
+        <?php include 'menu.inc.php' ?>
+		<div id="cadastro">
+			<h2>Preencha os seguintes campos para prosseguir o cadastro</h2>
+			<form>
+			<label>Nome de usuário: </label><input type="text">
+			<br>
+			<br>
+			<label>Senha: </label><input type="password">
+			<br>
+			<br>
+			<label>O que você é?</label>
+				<select>
+					<option>Carente</option>
+					<option>Voluntário</option>
+					<option>Doador</option>
+				</select>
+            <input type="submit" value="Enviar" class="botao">    
+			</form>
+		</div>
+		<img src="cadastro.png"></img>
+	</body>
+</html>
---
+++ cadastroCarente.inc.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="estiloCadastro.css">
+	</head>
+	<body>
+        <?php include 'menu.inc.php' ?>
+		<div id="cadastro">
+			<h2>Carente, preencha os seguintes campos para efetuar o cadastro</h2>
+			<form>
+			<label>Nome completo: </label><input type="text">
+			<br>
+			<br>
+			<label>Telefone: </label><input type="number" min="0">
+			<br>
+			<br>
+			<label>Renda:</label>
+                <input type="radio" name="r" value="1"><div id="renda1">Entre R$ e</div><br>
+                <input type="radio" name="r" value="2"><div id="renda2">Entre R$ e</div><br>
+                 <input type="radio" name="r" value="3"><div id="renda3">Entre R$ e</div><br>
+            <br>    
+            <label>RG: </label><input type="number" min="0">
+			<br>
+			<br>   
+            <label>Email: </label><input type="text">
+			<br> 
+            <input type="submit" value="Enviar" class="botao">    
+			</form>
+		</div>
+		<img src="fome.jpg"></img>
+    </body>
+</html>
